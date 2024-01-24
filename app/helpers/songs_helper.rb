90c3d940fd87b96f1d720ba6f8ff2dc7bb51ba28
katstasaph <katherinestasaph@gmail.com>
2024-01-24 10:29:43 -0500

    add site FAQ, fix cosmetic issues on song index page

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,8 @@ module SongsHelper
     { 
     songs: true, 
 	empty: song.status == "open" && blurbs == 0, 
-	blurbed: song.status == "open" && blurbs > 0 && blurbs < 11,
+	blurbed: song.status == "open" && blurbs > 0 && blurbs < 6,
+	ready: song.status == "open" && blurbs  >= 6 && blurbs < 11,
 	full: song.status == "open" && blurbs > 11,
 	closed: song.status == "closed",
 	published: song.status == "published"
