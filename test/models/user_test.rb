49f3d393edc3f43c39d3403252237f294bf50c84
Kat Stevens <kat.stevens@gmail.com>
2024-01-27 21:35:53 +0000

    KS: Unit tests for User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User creation happy path" do
+    user = User.new(username: "georgeeliot", name: "George Eliot", password_confirmation: "whatevs")
+    assert user.valid?
+  end
+
+  test "User creation invalid without username" do
+    user = User.new(name: "George Eliot", password_confirmation: "whatevs")
+    assert_not user.valid?
+  end
+
+  test "User creation invalid without password confirmation" do
+    user = User.new(username: "georgeeliot", name: "George Eliot")
+    assert_not user.valid?
+  end
+
+  test "User creation requires unique email address" do
+    user1 = User.create(username: "georgeeliot", password_confirmation: "whatevs", email: "george@example.com")
+    user2 = User.new(username: "georgeconstanza", password_confirmation: "whatevs",  email: "george@example.com")
+    assert_not user2.valid?
+  end
 end
