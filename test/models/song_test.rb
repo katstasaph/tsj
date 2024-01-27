d6a868f8af5b5df0d3820264534ecd6684ac45f2
Kat Stevens <kat.stevens@gmail.com>
2024-01-27 18:03:10 +0000

    KS: Basic model tests for Song class

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class SongTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "song is valid with only title and artist" do
+    song = Song.new(title: "Connection", artist: "Elastica")
+    assert song.valid?
+  end
+
+  test "song is invalid without title or artist" do
+    song = Song.new(title: "Connection")
+    assert_not song.valid?
+
+    song = Song.new(artist: "Elastica")
+    assert_not song.valid?
+  end
+
+  test "song requires status before being saved" do
+    song = Song.create(title: "Car Song", artist: "Elastica", status: "open")
+    assert song.valid?
+    song.save
+    assert_equal(song.status, "open")
+  end
 end
