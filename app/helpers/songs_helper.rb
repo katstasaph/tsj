f39d34ea1c75410e28b943809e9b8e4aba8446d2
katstasaph <katherinestasaph@gmail.com>
2024-02-01 01:21:30 -0500

    minor style tweaks/page text updates

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -5,7 +5,7 @@ module SongsHelper
 	empty: song.status == "open" && blurbs == 0, 
 	blurbed: song.status == "open" && blurbs > 0 && blurbs < 6,
 	ready: song.status == "open" && blurbs  >= 6 && blurbs < 11,
-	full: song.status == "open" && blurbs > 11,
+	full: song.status == "open" && blurbs >= 11,
 	closed: song.status == "closed",
 	published: song.status == "published"
 	}
