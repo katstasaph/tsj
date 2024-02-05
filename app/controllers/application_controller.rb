db44b5799208fb2d1fff9892776ca01d29ecb0cd
katstasaph <katherinestasaph@gmail.com>
2024-02-05 12:30:52 -0500

    hopefully fixes issue with ActiveStorage URLs, not sure why this wasn't an issue earlier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  include ActiveStorage::SetCurrent
+
   private
 
   def user_not_authorized
