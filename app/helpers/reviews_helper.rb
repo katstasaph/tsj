f29652f38aff80bdc34d0949f4da61b49f7e6f0b
katstasaph <katherinestasaph@gmail.com>
2024-02-12 20:30:31 -0500

    Added inline editing of reviews from song page

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,6 @@
 module ReviewsHelper
+  def inline_review_edit?
+    referrer = Rails.application.routes.recognize_path(request.referrer) 
+	referrer[:controller] == "songs" && referrer[:action] == "show"
+  end
 end
