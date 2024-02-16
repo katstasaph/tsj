1d21bb8e33d347cb62563f7ae3beca985a56f75f
Kat Stevens <kat.stevens@gmail.com>
2024-02-16 20:40:21 +0000

    KS: Unit and system tests for new song flash message

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -10,7 +10,26 @@ class SongsControllerTest < ActionDispatch::IntegrationTest
 
   test "authenticated user should see index" do
     sign_in users(:bob)
-    get "/"
+    get root_path
     assert_response :success
+  end
+
+  test "admin can add new song" do
+    sign_in users(:bob)
+    get new_song_path
+    assert_response :success
+
+    post songs_path, params: { song: { artist: 'Patti Smith', title: 'Horses' } }
+    follow_redirect!
+    assert_response :success
+    assert_select "p.notice", "Added song!"
+  end
+
+  test "writer cannot add new song" do
+    sign_in users(:alice)
+    get new_song_path
+    assert_response :redirect
+    follow_redirect!
+    assert_select "p.alert", text: "You are not authorized to perform this action."
   end
 end
