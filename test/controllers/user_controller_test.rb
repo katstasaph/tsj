4f97f13a3e5d8bffe28966d3a7089688edb8f509
Kat Stevens <kat.stevens@gmail.com>
2024-03-02 21:09:13 +0000

    KS: Add test to check Edit link not shown for published songs

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -14,6 +14,7 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     # Both reviews should be visible
     assert_select "strong", text: "Boney M - Rasputin"
     assert_select "strong", text: "Wombo - Slab"
+    assert_select "strong", text: "Sky Ferreira - Everything Is Embarrassing"
   end
 
   test "unpublished reviews only if query param present " do
@@ -24,8 +25,19 @@ class UserControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     assert_select "p.show_unpublished_link", "Show all reviews"
-    # Should only be 1 review visible as Slab is 'closed'
+    # Should only be 1 review visible as Slab and Sky are closed/published
     assert_select "strong", text: "Boney M - Rasputin"
     assert_select "strong", text: "Wombo - Slab", count: 0
+    assert_select "strong", text: "Sky Ferreira - Everything Is Embarrassing", count: 0
+  end
+
+  test "published reviews appear by default but do not show edit link" do
+    sign_in users(:alice)
+    @user = User.first
+
+    get user_path(@user.id)
+
+    assert_select "strong", text: "Sky Ferreira - Everything Is Embarrassing"
+    assert_select "a", text: "Edit", count: 2
   end
 end
