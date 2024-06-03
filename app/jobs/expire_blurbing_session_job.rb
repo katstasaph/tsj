139cca538881bec784119f026326a422a312163a
katstasaph <katherinestasaph@gmail.com>
2024-06-03 12:14:04 -0400

    oops

--- app/jobs/expire_blurbing_session_job.rb
+++ app/jobs/expire_blurbing_session_job.rb
@@ -2,7 +2,7 @@ class ExpireBlurbingSessionJob < ApplicationJob
   queue_as :default
 
   def perform(session_id)
-    session = ActiveBlurbing.find_by(session_id)
+    session = ActiveBlurbing.find_by(id: session_id)
     if session
       session.destroy
     end
